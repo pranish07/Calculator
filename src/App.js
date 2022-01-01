33a48e59742a3fede3c2dcc5c0878b3fef5d96e9
Pranish Shrestha <pranishshrestha181@gmail.com>
2022-01-01 18:03:18 +0545

    html added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        I am pranish.
-        </p>
-        
-      </header>
+     <div className ="output">
+       <div className="previous-output"></div>
+       <div className ="current-output"></div>
+     </div>
+
+     <button className ="span-two">AC</button>
+     <button>DEL</button>
+     <button>/</button>
+     <button>1</button>
+     <button>2</button>
+     <button>3</button>
+     <button>*</button>
+     <button>4</button>
+     <button>5</button>
+     <button>6</button>
+     <button>+</button>
+     <button>7</button>
+     <button>8</button>
+     <button>9</button>
+     <button>-</button>
+     <button>.</button>
+     <button>0</button>
+     <button className ="span-two">=</button>
+     
     </div>
   );
 }
